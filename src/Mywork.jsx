ec4437d21a0518536cec276fd25789dad9bb1290
Juan Marcos Lorenzo <jmlorenzo1993u@gmail.com>
2024-02-11 20:07:33 -0300

    correcciones en mywork

--- src/Mywork.jsx
+++ src/Mywork.jsx
@@ -3,6 +3,7 @@ import './mywork.css'
 export function MyWork(){
     return(
         <>
+        <main>
             <section className="mywork-header">
                 <h2 className="mywork-header-title">My work</h2>
                 <span>para ver mas ejemplos</span><a href="mailto:jmlorenzo1993@gmail.com">Email me</a>
@@ -12,7 +13,7 @@ export function MyWork(){
                 <div className="mywork-first-project-container-info">
                     <h2 className="mywork-first-project-title">GLC Solutions</h2>
                     <li>UX/UI design, wordpress theme customization</li>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia architecto modi impedit distinctio. Inventore, numquam reiciendis quis repudiandae delectus aperiam minus eveniet obcaecati voluptates totam doloremque alias nihil soluta distinctio?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br />Mollitia architecto modi impedit distinctio.</p>
                     <button>visit website</button>
                 </div>
                 <div className='mywork-first-project-container-img-container'>
@@ -29,7 +30,7 @@ export function MyWork(){
                 <div className="mywork-second-project-container-info">
                     <h2 className="mywork-second-project-title">GLC Solutions</h2>
                     <li>UX/UI design, wordpress theme customization</li>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia architecto modi impedit distinctio. Inventore, numquam reiciendis quis repudiandae delectus aperiam minus eveniet obcaecati voluptates totam doloremque alias nihil soluta distinctio?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br />Mollitia architecto modi impedit distinctio.</p>
                     <button>visit website</button>
                 </div>
             </section>
@@ -40,7 +41,7 @@ export function MyWork(){
                 <div className="mywork-first-project-container-info">
                     <h2 className="mywork-first-project-title">GLC Solutions</h2>
                     <li>UX/UI design, wordpress theme customization</li>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia architecto modi impedit distinctio. Inventore, numquam reiciendis quis repudiandae delectus aperiam minus eveniet obcaecati voluptates totam doloremque alias nihil soluta distinctio?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br />Mollitia architecto modi impedit distinctio.</p>
                     <button>visit website</button>
                 </div>
                 <div className='mywork-first-project-container-img-container'>
@@ -48,6 +49,7 @@ export function MyWork(){
                 </div>
             </section>
             <hr />
+        </main>
         </>
     )
 }
